2a6d902273edfdfe3148473d1c614551700bb4d7
Damien Eversmann <damien.eversmann@gmail.com>
2021-09-10 20:17:27 -0500

    Simplify config and refactor a little

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
 	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -13,61 +13,62 @@ import (
 
 type Contact struct {
 	ID        uint   `json:"id" gorm:"primary_key, AUTO_INCREMENT"`
-	FirstName string `json:"firstname"`
-	LastName  string `json:"lastname"`
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
 	Address   string `json:"address"`
 	City      string `json:"city"`
 	State     string `json:"state"`
-	ZipCode   string `json:"zipcode"`
+	Zipcode   string `json:"zipcode"`
 	Phone     string `json:"phone"`
 	Email     string `json:"email"`
 }
 
-var sampleCon = Contact{FirstName: "Paul", LastName: "Cormir", Address: "100 E. Davie St.", City: "Raleigh", State: "NC", ZipCode: "27601", Phone: "888-RED-HAT-1", Email: "pcormir@redhat.com"}
-var db *gorm.DB
-
-func main() {
-	viper.SetDefault("DBDialect", "sqlite")
-	viper.SetDefault("DBDSN", "file::memory:?cache=shared")
-	viper.SetDefault("Port", "8080")
-	viper.SetConfigName("goaddr-config")
-	viper.AddConfigPath(".")
-	viper.SetEnvPrefix("goaddr")
-	viper.AutomaticEnv()
-	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			// Config file not found; ignore error if desired
-		} else {
-			// Config file was found but another error was produced
-			panic(fmt.Errorf("fatal error config file: %w", err))
-		}
-	}
-
-	for k, v := range viper.AllSettings() {
-		fmt.Printf("  %s=%s\n", k, v)
-	}
-
-	r := gin.Default()
-
-	connectDatabase()
-
-	gin.EnableJsonDecoderDisallowUnknownFields() // error on undefined JSON fields
-	v1 := r.Group("/api/v1/contacts")
-	{
-		v1.POST("", createContact)
-		v1.GET("", readContacts)
-		v1.GET("/:id", readContact)
-		v1.PUT("/:id", updateContact)
-		v1.DELETE("/:id", deleteContact)
-	}
-	r.Run(fmt.Sprintf(":%s", viper.Get("Port")))
+var sampleCon = Contact{
+	Firstname: "Paul",
+	Lastname:  "Cormir",
+	Address:   "100 E. Davie St.",
+	City:      "Raleigh",
+	State:     "NC",
+	Zipcode:   "27601",
+	Phone:     "888-RED-HAT-1",
+	Email:     "pcormir@redhat.com",
 }
 
-func connectDatabase() {
-	var dialector gorm.Dialector
-	dialect := viper.Get("DBDialect").(string)
-	dsn := viper.Get("DBDSN").(string)
+var db *gorm.DB
 
+// default values will be overwritten as needed
+var dialect = "sqlite"
+var dsn = "file::memory:?cache=shared"
+var port = "8080"
+
+func main() {
+
+	initConfig()
+	initDB()
+
+	r := gin.Default()
+	gin.EnableJsonDecoderDisallowUnknownFields() // error on undefined JSON fields
+	apiRoot := r.Group("/api")
+	v1 := apiRoot.Group("/v1")
+	contactsRoute := v1.Group("/contacts")
+	{
+		contactsRoute.POST("", createContact)
+		contactsRoute.GET("", readContacts)
+		contactsRoute.GET("/:id", readContact)
+		contactsRoute.PUT("/:id", updateContact)
+		contactsRoute.DELETE("/:id", deleteContact)
+	}
+	r.Run(port)
+}
+
+func initConfig() {
+	dialect = getenv("GOADDR_DBDIALECT", dialect)
+	dsn = getenv("GOADDR_DBDSN", dsn)
+	port = getenv("GOADDR_PORT", port)
+}
+
+func initDB() {
+	var dialector gorm.Dialector
 	switch dialect {
 	case "sqlite":
 		dialector = sqlite.Open(dsn)
@@ -155,4 +156,12 @@ func deleteContact(c *gin.Context) {
 
 	db.Delete(con)
 	c.Status(http.StatusNoContent)
+}
+
+func getenv(key, fallback string) string {
+	val := os.Getenv(key)
+	if len(val) == 0 {
+		return fallback
+	}
+	return val
 }
